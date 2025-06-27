b587149cec0e68e1256b3b562f907aa75b80b1b8
Annafee-Pathan <pathanannafee@gmail.com>
2025-06-27 12:47:37 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+first_name="Codingal"
+last_name="Educations"
+full_name=first_name+last_name
+ead='ha'*5
+print('Frist Name',first_name)
+print('Last Name',last_name)
+print('Full Name',full_name)
+print('String multiplied by five',ead)
+
+word='Coding'
+print('Length of string',len(word))
+print('First letter of string',word[0])
+print('Last letter of string',word[5])
+print('Sreing sliced',word[0:4])
