2a01c0579d2e4da686bb8117bd5a7666eed9b4ed
Annafee-Pathan <pathanannafee@gmail.com>
2025-07-05 12:39:39 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+n1=int(input("Enter Number 1 :"))
+n2=int(input("Enter Number 2 :"))
+
+print("Sum:",add(n1,n2))
+print("Difference:",subtract(n1,n2))
+print("Product:",multiply(n1,n2))
+print("Quotient:",divide(n1,n2))
