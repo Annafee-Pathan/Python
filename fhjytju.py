5d13f5ceb3a7408c24950adb210c8a4a902e138e
Annafee-Pathan <pathanannafee@gmail.com>
2025-07-18 12:51:46 +0600

    Add files via upload

---
+++ fhjytju.py
@@ -0,0 +1,8 @@
+setx={"green", "blue"}
+sety={"blue", "yellow","green"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\n union of two said sets:")
+setz=sety.union(setx)
+print(setz)
